82bed2d40cc6a641d535a553eefd4f8a196bc406
Turgutalp Tug <turgut7alp@gmail.com>
2020-08-22 01:21:31 +0300

    Challange_Added

---
+++ app/src/main/java/com/example/dicerollerchallenge/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.dicerollerchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{ rollDice()}
+        val rollButton_2 :Button = findViewById(R.id.challenge_button)
+        rollButton_2.setOnClickListener{challenge()}
+    }
+
+    private fun rollDice(){
+        Toast.makeText(this,"button clicked",
+                Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+        val randomInt = (1..6).random()
+        resultText.text = randomInt.toString()
+    }
+
+    private fun challenge(){
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        if(resultText.text == "Roll Dice!"){
+            resultText.text = "1"
+        }else{
+            var resultInt = resultText.text.toString().toInt()
+
+            if(resultInt <6){
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
+
+    }
+}
